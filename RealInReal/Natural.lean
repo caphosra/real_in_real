a7b1cea22c9a4281b96eca18b33b02f274636525
Akihisa Yagi <caphosra@gmail.com>
2025-07-11 11:00:53 +0900

    Define Rational

--- RealInReal/Natural.lean
+++ RealInReal/Natural.lean
@@ -1,2 +1,39 @@
 def Coprime(a:Int)(b: Nat): Prop :=
   ∀d, d ∣ a → d ∣ b → d = 1
+
+theorem zero_or_not: ∀d: Int, d ≠ 0 ∨ d = 0 := by
+  intros d
+  induction d
+  case ofNat d' =>
+    simp
+    induction d'
+    case zero =>
+      simp
+    case succ d'' h =>
+      simp
+      have: ∀a: Nat, (↑a: Int) ≥ 0 := by
+        simp
+      let d''_not_neg := this d''
+      have: (↑d'': Int) + 1 = 0 → False := by
+        intros h
+        have d_minus_one: (↑d'': Int) = -1 := by
+          have: (↑d'': Int) + 1 - 1 = 0 - 1 := by
+            rw [h]
+          simp at this
+        simp [d_minus_one] at d''_not_neg
+      exact Or.inl this
+  case negSucc d' =>
+    simp
+
+theorem divisible_or_zero: ∀a b d: Int, d * a = d * b → a = b ∨ d = 0 := by
+  intros a b d h
+  let d_zero_or_not := zero_or_not d
+  cases d_zero_or_not
+  case inl d_neq_zero =>
+    rw [Int.mul_eq_mul_left_iff d_neq_zero] at h
+    exact Or.inl h
+  case inr d_eq_zero =>
+    exact Or.inr d_eq_zero
+
+#print axioms zero_or_not
+#print axioms divisible_or_zero
--- RealInReal/Rational.lean
+++ RealInReal/Rational.lean
@@ -1,15 +1,63 @@
 import RealInReal.Natural
 
-structure Rational where
+structure NumPair where
   num: Int
   denom: Nat
 
   denom_non_zero: denom ≠ 0
-  denom_eq_one_if_num_eq_zero: num = 0 → denom = 1
-  num_denom_coprime: num ≠ 0 → Coprime num denom
 
-instance : LE Rational where
+instance : LE NumPair where
   le a b := a.num * b.denom ≤ b.num * a.denom
 
-instance : LT Rational where
+instance : LT NumPair where
   lt a b := a.num * b.denom < b.num * a.denom
+
+instance NumPairSetoid : Setoid NumPair where
+  r := fun a b => a.num * b.denom = b.num * a.denom
+  iseqv := by
+    constructor
+    case refl =>
+      intros x
+      rfl
+    case symm =>
+      intros x y h
+      rw [Int.mul_comm]
+      rw [h]
+      rw [Int.mul_comm]
+    case trans =>
+      intros x y z h1 h2
+      have h1_mod: x.num * ↑y.denom * ↑z.denom = y.num * ↑x.denom * ↑z.denom := by
+        rw [h1]
+      have h2_mod: y.num * ↑z.denom * ↑x.denom = z.num * ↑y.denom * ↑x.denom := by
+        rw [h2]
+      have: y.num * ↑x.denom * ↑z.denom = y.num * ↑z.denom * ↑x.denom := by
+        rw [Int.mul_assoc]
+        rw [Int.mul_assoc]
+        conv =>
+          lhs
+          conv =>
+            rhs
+            rw [Int.mul_comm]
+      have: y.denom * (x.num * ↑z.denom) = y.denom * (z.num * ↑x.denom) := by
+        rw [←h1_mod] at this
+        rw [h2_mod] at this
+        conv at this =>
+          lhs
+          conv =>
+            lhs
+            rw [Int.mul_comm]
+          rw [Int.mul_assoc]
+        conv at this =>
+          rhs
+          conv =>
+            lhs
+            rw [Int.mul_comm]
+          rw [Int.mul_assoc]
+        exact this
+      have y_denom_zero: (↑y.denom: Int) ≠ 0 := by
+        rw [Int.ofNat_ne_zero]
+        exact y.denom_non_zero
+      rw [Int.mul_eq_mul_left_iff y_denom_zero] at this
+      exact this
+
+def Rational := Quotient NumPairSetoid
